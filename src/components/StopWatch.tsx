ae063aabaf6a86dadf004f79d69e7f99141ca427
ramkumarsethu <66165800+ramkumarsethu@users.noreply.github.com>
2024-03-21 17:11:13 +0900

    refactored code

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,16 +1,21 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import StopWatchButton from './Button';
 
-const resetValue = '00:00:00.00';
+const defaultValue: { hours: number; minutes: number; seconds: number; milliSeconds: number } = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+  milliSeconds: 0
+};
 
 const StopWatch = () => {
-  const displayRef = useRef<{ counterString: string }>({ counterString: resetValue });
   const timerIntervalRef = useRef<{ intervalRef: ReturnType<typeof setInterval> }>({
     intervalRef: 0
   });
-  const minutesHoursRef = useRef<{ hours: number; minutes: number }>({ hours: 0, minutes: 0 });
+  const [minutesHours, setMinutesHours] = useState({ ...defaultValue });
   const [timer, setTimer] = useState(0);
   const [startCounter, setStartCounter] = useState(false);
+  const [resetPressed, setResetPressed] = useState(false);
 
   const startStopCounter = useCallback((startCounter: boolean, resetCounter?: boolean) => {
     if (startCounter) {
@@ -20,16 +25,18 @@ const StopWatch = () => {
       timerIntervalRef.current.intervalRef = intervalNumber;
     } else if (resetCounter) {
       clearInterval(timerIntervalRef.current.intervalRef);
-      setTimer(0);
-      minutesHoursRef.current = {
-        hours: 0,
-        minutes: 0
-      };
-      displayRef.current.counterString = resetValue;
+      setResetPressed(true);
     } else {
       clearInterval(timerIntervalRef.current.intervalRef);
     }
   }, []);
+
+  useEffect(() => {
+    if (resetPressed) {
+      setMinutesHours({ ...defaultValue });
+      setTimer(0);
+    }
+  }, [resetPressed]);
 
   const padTwoDigits = useCallback((digits: string) => {
     if (digits) {
@@ -45,33 +52,32 @@ const StopWatch = () => {
   }, [startStopCounter]);
 
   const startButtonHandler = useCallback(() => {
+    setResetPressed(false);
     startStopCounter(!startCounter);
     setStartCounter((startFlag) => !startFlag);
   }, [startCounter, startStopCounter]);
 
   useEffect(() => {
-    if (displayRef.current) {
-      const minutes = Math.floor(timer / 6000);
-      if (minutes + minutesHoursRef.current.minutes > minutesHoursRef.current.minutes) {
-        const newMinutes = minutesHoursRef.current.minutes + 1;
-        if (newMinutes === 60) {
-          minutesHoursRef.current.hours += 1;
-          minutesHoursRef.current.minutes = 0;
-        } else {
-          minutesHoursRef.current.minutes = newMinutes;
-        }
-        setTimer(0);
+    const minutes = Math.floor(timer / 6000);
+    if (minutes + minutesHours.minutes > minutesHours.minutes) {
+      const newMinutes = minutesHours.minutes + 1;
+      if (newMinutes === 60) {
+        setMinutesHours((oldValue) => ({ ...oldValue, hours: oldValue.hours + 1, minutes: 0 }));
+      } else {
+        setMinutesHours((oldValue) => ({ ...oldValue, minutes: newMinutes }));
       }
+      setTimer(0);
+    } else {
       const seconds = (timer / 100.0).toString().split('.');
-      displayRef.current.counterString = `${padTwoDigits(
-        minutesHoursRef.current.hours.toString() //displaying hours
-      )}:${padTwoDigits(
-        minutesHoursRef.current.minutes.toString() //displaying minutes
-      )}:${padTwoDigits(
-        seconds[0] //displaying seconds
-      )}.${padTwoDigits(seconds[1])}`; //displaying milliseconds
+      setMinutesHours((oldValue) => {
+        return {
+          ...oldValue,
+          seconds: seconds[0] ? Number(seconds[0]) : 0,
+          milliSeconds: seconds[1] ? Number(seconds[1]) : 0
+        };
+      });
     }
-  }, [timer, padTwoDigits]);
+  }, [timer, padTwoDigits, minutesHours.minutes]);
 
   return (
     <div
@@ -82,10 +88,28 @@ const StopWatch = () => {
         flexDirection: 'column',
         height: '100vh',
         rowGap: 20,
-        backgroundColor: 'beige'
+        backgroundColor: 'beige',
       }}>
-      <div style={{ fontSize: 50 }}>{displayRef.current.counterString}</div>
-      <div style={{ display: 'flex', justifyContent: 'space-around', width: '50vw' }}>
+      <div style={{ fontSize: 50 }}>
+        <>
+          {padTwoDigits(
+            minutesHours.hours.toString() //displaying hours
+          )}
+          :
+          {padTwoDigits(
+            minutesHours.minutes.toString() //displaying minutes
+          )}
+          :
+          {padTwoDigits(
+            minutesHours.seconds.toString() //displaying seconds
+          )}
+          .
+          {padTwoDigits(
+            minutesHours.milliSeconds.toString() //displaying milli seconds
+          )}
+        </>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-around', width: '50vw', columnGap: 10 }}>
         <StopWatchButton
           onClickHandler={resetButtonHandler}
           buttonClass="btn-secondary"
